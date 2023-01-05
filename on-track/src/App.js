f32dcb20c8e8be7d72ba0d57a0d17f02358a99b5
ADogTheDestroyer <andrewhlin0@gmail.com>
2023-01-04 22:33:44 -0500

    First step of the app

---
+++ on-track/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePageComponent from './page-components/HomePageComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePageComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ on-track/src/page-components/HomePageComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function HomePageComponent() {
+  return (
+    <div className='homeMenuContainer'>
+      <div className='homeTitleContainer'>
+        <h1>Welcome to ON-TRACK</h1>
+        <h2>Let's not waste any time</h2>
+      </div>
+      <button className='homePageButton'>Today</button>
+      <button className='homePageButton'>Set Goals</button>
+      <button className='homePageButton'>Track Progress</button>
+    </div>
+  )
+}
+
+export default HomePageComponent
