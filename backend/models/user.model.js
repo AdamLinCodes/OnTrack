fd18a01800dd7a46231f3522fc8ccee4317e3237
ADogTheDestroyer <andrewhlin0@gmail.com>
2023-01-07 01:31:04 -0500

    added starter backend

---
+++ backend/models/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,     //data type
+    required: true,   //cant be empty
+    unique: true,     //must be unique
+    trim: true,       //gets rid of extra spaces
+    minlength: 3      //must be at least 3 characters
+  },
+}, {
+  timestamps: true,   //will include a field for when it was made/updated
+});
+
+const User = mongoose.model('User', userSchema);  //instantiate
+
+module.exports = User;  //export so we can reference it in other files
