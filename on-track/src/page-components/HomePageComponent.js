875e187eee9412d6ec625fd96eb112ccff37e27e
ADogTheDestroyer <andrewhlin0@gmail.com>
2023-01-06 23:40:52 -0500

    More style

--- on-track/src/page-components/HomePageComponent.js
+++ on-track/src/page-components/HomePageComponent.js
@@ -8,9 +8,9 @@ function HomePageComponent() {
         <h2>Welcome!</h2>
         <h2>Let's not waste any time</h2>
       </div>
-      <button className='homePageButton'>Today</button>
-      <button className='homePageButton'>Set Goals</button>
-      <button className='homePageButton'>Track Progress</button>
+      <div className='homePageButtonContainer'><button className='homePageButton'>Today</button></div>
+      <div className='homePageButtonContainer'><button className='homePageButton'>Set Goals</button></div>
+      <div className='homePageButtonContainer'><button className='homePageButton'>Track Progress</button></div>
     </div>
   )
 }
