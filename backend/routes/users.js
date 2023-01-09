bdad7cc39f8df9c07dc2e6be22fc4cb0423cedcc
ADogTheDestroyer <andrewhlin0@gmail.com>
2023-01-09 15:38:04 -0500

    backend runs on port 8080 to support apple monterey

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -18,4 +18,20 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/checkuser').get( (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({username: username, password: password}, (err, user) => {
+    if (err) res.status(400).json('Sorry, unable to process the request');
+
+    if (user == null) {
+      res.status(200).json(false);
+    }else {
+      const userExists = (username === user.username) && (password === user.password);
+      res.status(200).json(userExists);
+    }
+  });
+});
+
 module.exports = router;
