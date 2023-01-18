50aa380e27b79ba212a064e2ffd1f75748251769
Kyle Newbigging <kyle.newbigging@gmail.com>
2023-01-17 19:11:45 -0500

    Edit Page and form initial creation

---
+++ on-track/src/other-components/EditForm.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../styles/edit.css';
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+/*  console.log(`Username: ${username}`);
+    console.log(`Password: ${password}`); */
+
+    // validate form values and submit them to the server
+    try {
+        const response = await axios.post('http://localhost:8080/users/checkuser', {
+          username: username,
+          password: password
+        });
+        const validUser = response.data;
+
+        console.log(username + ', ' + password + ', ' + validUser);
+
+        if(validUser === true) {
+          document.location.href = '/home';
+        } else {
+          setUsername('');
+          setPassword('');
+          document.location.reload();
+        }
+      } catch (error) {
+        console.error(error);
+      }
+  };
+
+  return (
+    <div>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={event => setUsername(event.target.value)}
+        />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+        <br />
+        <div className="buttonContainer">
+          <button className="button" type="submit">Log in</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
--- on-track/src/page-components/EditPage.js
+++ on-track/src/page-components/EditPage.js
@@ -2,14 +2,15 @@ import React from 'react';
 import '../styles/edit.css';
 
 function EditPage() {
+
+    
   return (
     <div className='menuContainer'>
       <div className='titleContainer'>
         <h1>ON-TRACK</h1>
-        <h2>Welcome!</h2>
-        <h2>Let's not waste any time</h2>
+        <h2>Edit your Tasks!</h2>
       </div>
-      <p>THIS IS THE EDIT PAGE!!</p>
+      <EditForm/>
     </div>
   )
 }
