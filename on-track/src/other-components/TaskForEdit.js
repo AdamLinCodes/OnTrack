2bc8d5647f18af3c059448b858045d68907ce108
ADogTheDestroyer <andrewhlin0@gmail.com>
2023-01-29 09:23:37 -0500

    Edit page is now styled

--- on-track/src/other-components/TaskForEdit.js
+++ on-track/src/other-components/TaskForEdit.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import editTaskImage from '../assets/editTask.png';
+import removeTaskImage from '../assets/removeTask.png';
 
 function TaskForEdit() {
   return (
-    <div className="task">
-      <p className="taskName">Task Example</p>
-      <p className="taskDuration">2 hours</p>
+    <div>
+      <img src={removeTaskImage} alt="RemoveImage" />
+      <img src={editTaskImage} alt="EditImage" />
+      <div className="task">
+        <p className="taskName">Task Example</p>
+        <p className="taskDuration">2 hours</p>
+      </div>
     </div>
   )
 }
